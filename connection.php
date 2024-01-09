3ba8ad6d127f275c8a2ebdb402f4ab8107b16faa
arsalan sameer <132910781+arsaalan786@users.noreply.github.com>
2024-01-09 10:43:57 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$conn=mysqli_connect("localhost","id20325099_user123","Arsalanuser@123","id20325099_userdata");
+if(!$conn){
+    echo "Not connected....";
+}else{
+    echo "Connection Successfully...";
+}
+?>
